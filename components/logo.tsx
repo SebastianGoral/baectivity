22f3cfb4698c37ce38e0de5dd993cd3fc7e26dd4
sebastiangoral <sebastian.goral@fundingport.com>
2022-10-14 23:58:33 +0200

    init styles

---
+++ components/logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.span`
+  font-family: "Caveat", cursive;
+  font-family: "Mali", cursive;
+  margin: 0 auto;
+  font-size: 50px;
+
+  color: rgb(101, 48, 4);
+`;
+
+export const Logo = () => {
+  return <Wrapper>baectivity</Wrapper>;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
+import "../styles/styles.css";
+import { initThinBackend } from "thin-backend";
+import { ThinBackend } from "thin-backend-react";
+
+initThinBackend({
+  host: "https://baectivity.thinbackend.app",
+});
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThinBackend>
+      <Component {...pageProps} />
+    </ThinBackend>
+  );
 }
 
-export default MyApp
+export default MyApp;
